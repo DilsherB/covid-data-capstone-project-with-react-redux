96e0e6589f07286240bf136be49e1014a6be1910
Dilsher Balouch <dilsherb@gmail.com>
2023-06-26 23:24:54 +0300

    APIData component completed.

--- src/redux/APIData.js
+++ src/redux/APIData.js
@@ -1,2 +1,42 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
+
+const URL_CONTINENTS = "https://disease.sh/v3/covid-19/continents";
+const URL_CONTRIES = "https://disease.sh/v3/covid-19/countries";
+const URL_ALL = "https://disease.sh/v3/covid-19/all";
+
+export const fetchContinents = createAsyncThunk(
+  "APIData/fetchContinents",
+  async () => {
+    try {
+      const response = await axios.get(URL_CONTINENTS);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return "something went wrong!";
+    }
+  }
+);
+
+export const fetchCountries = createAsyncThunk(
+  "APIData/fetchCountries",
+  async () => {
+    try {
+      const response = await axios.get(URL_CONTRIES);
+      console.log(response.data);
+      return response.data;
+    } catch (error) {
+      return "something went wrong!";
+    }
+  }
+);
+
+export const fetchAll = createAsyncThunk("APIData/fetchAll", async () => {
+  try {
+    const response = axios.get(URL_ALL);
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    return "something went wrong!";
+  }
+});
