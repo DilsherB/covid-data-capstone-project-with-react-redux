0819e85ba8388b7162d9eda4e959ab6a1b73274f
Dilsher Balouch <dilsherb@gmail.com>
2023-06-27 23:49:43 +0300

    half styling done for country page.

--- src/components/Country.js
+++ src/components/Country.js
@@ -11,19 +11,39 @@ const Country = () => {
     dispatch(fetchCountries());
   }, [dispatch]);
   return (
-    <div>
-      <h1>Country Page</h1>
+    <div className="grid md:grid-cols-2 mx-5 gap-10 mb-10">
       {loading && <h3>Loading...</h3>}
       {error && <h3>{error}</h3>}
       {countries.map((country) => {
         return (
           <div key={country.country}>
-            <h2>Country: {country.country}</h2>
-            <p>Continent: {country.continent}</p>
-            <p>Population: {country.population}</p>
-            <p>Total Covid Cases: {country.cases}</p>
-            <p>Deaths due to Covid: {country.deaths}</p>
-            <img alt="Country's Flag" src={country.countryInfo.flag} />
+            <div className="flex gap-3 mb-3">
+              <div className="bg-slate-200 w-1/3 flex items-center justify-center p-3">
+                <img
+                  width={200}
+                  className="rounded"
+                  alt="Country's Flag"
+                  src={country.countryInfo.flag}
+                />
+              </div>
+              <div className="text-left">
+                <h2 className="text-2xl md:text-3xl underline mb-5">
+                  Country: {country.country}
+                </h2>
+                <p>Population: {country.population}</p>
+                <p>Continent: {country.continent}</p>
+              </div>
+            </div>
+            <div className="p-3 grid grid-cols-2 text-left bg-slate-300 rounded">
+              <p>Total Cases: {country.cases}</p>
+              <p>Total Recovered: {country.recovered}</p>
+              <p>Today Cases: {country.todayCases}</p>
+              <p>Today Recovered: {country.todayRecovered}</p>
+              <p>Deaths: {country.deaths}</p>
+              <p>Critical: {country.critical}</p>
+              <p>Active: {country.active}</p>
+              <p>Today Deaths: {country.todayDeaths}</p>
+            </div>
           </div>
         );
       })}
