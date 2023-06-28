1127ea7daa85d2d5a2af110eef0e3e2ef546f7b2
Dilsher Balouch <dilsherb@gmail.com>
2023-06-28 07:43:29 +0300

    more styling done for country page.

--- src/components/Country.js
+++ src/components/Country.js
@@ -16,33 +16,63 @@ const Country = () => {
       {error && <h3>{error}</h3>}
       {countries.map((country) => {
         return (
-          <div key={country.country}>
-            <div className="flex gap-3 mb-3">
-              <div className="bg-slate-200 w-1/3 flex items-center justify-center p-3">
+          <div className="h-80 border-2 rounded p-5" key={country.country}>
+            <div className="flex gap-3">
+              <div className="bg-slate-200 h-32 w-1/3 flex items-center justify-center rounded p-3">
                 <img
-                  width={200}
+                  // width={200}
                   className="rounded"
                   alt="Country's Flag"
                   src={country.countryInfo.flag}
                 />
               </div>
-              <div className="text-left">
+              <div className="text-left h-40">
                 <h2 className="text-2xl md:text-3xl underline mb-5">
                   Country: {country.country}
                 </h2>
-                <p>Population: {country.population}</p>
-                <p>Continent: {country.continent}</p>
+                <p className="flex justify-between">
+                  <span>Population: </span>
+                  <span>{country.population}</span>
+                </p>
+                <p className="flex justify-between">
+                  <span>Continent: </span>
+                  <span>{country.continent}</span>
+                </p>
               </div>
             </div>
-            <div className="p-3 grid grid-cols-2 text-left bg-slate-300 rounded">
-              <p>Total Cases: {country.cases}</p>
-              <p>Total Recovered: {country.recovered}</p>
-              <p>Today Cases: {country.todayCases}</p>
-              <p>Today Recovered: {country.todayRecovered}</p>
-              <p>Deaths: {country.deaths}</p>
-              <p>Critical: {country.critical}</p>
-              <p>Active: {country.active}</p>
-              <p>Today Deaths: {country.todayDeaths}</p>
+            <div className="p-3 grid gap-x-20 grid-cols-2 text-left bg-slate-300 rounded">
+              <p className="flex justify-between">
+                <span>Total Cases: </span>
+                <span>{country.cases}</span>
+              </p>
+              <p className="flex justify-between">
+                <span>Total Recovered: </span>
+                <span>{country.recovered}</span>
+              </p>
+              <p className="flex justify-between">
+                <span>Today Cases: </span>
+                <span>{country.todayCases}</span>
+              </p>
+              <p className="flex justify-between">
+                <span>Today Recovered: </span>
+                <span>{country.todayRecovered}</span>
+              </p>
+              <p className="flex justify-between">
+                <span>Deaths: </span>
+                <span>{country.deaths}</span>
+              </p>
+              <p className="flex justify-between">
+                <span>Critical: </span>
+                <span>{country.critical}</span>
+              </p>
+              <p className="flex justify-between">
+                <span>Active: </span>
+                <span>{country.active}</span>
+              </p>
+              <p className="flex justify-between">
+                <span>Today Deaths: </span>
+                <span>{country.todayDeaths}</span>
+              </p>
             </div>
           </div>
         );
