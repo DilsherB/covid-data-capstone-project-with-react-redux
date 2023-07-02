90196c13a35700a25f7b60cab55571983d0d7edf
Dilsher <dilsherb@gmail.com>
2023-07-02 10:57:25 +0300

    show/hide detail of the country added.

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { FaClock } from "react-icons/fa";
 import { fetchCountries } from "../redux/countrySlice";
 import DateComponent from "../globals";
 
 const Country = () => {
+  const [show, setShow] = useState(false);
+  const [currentCuntary, setCurrentCuntary] = useState(null);
   const { countries, loading, error } = useSelector(
     (state) => state.countrySlice
   );
@@ -12,6 +14,15 @@ const Country = () => {
   useEffect(() => {
     dispatch(fetchCountries());
   }, [dispatch]);
+  const handleShow = (country) => {
+    setCurrentCuntary(country);
+    setShow(true);
+  };
+  const handleHide = () => {
+    // setHide(!hide);
+    setShow(false);
+  };
+
   return (
     <div>
       {loading && <h3 className="text-center">Loading...</h3>}
@@ -20,6 +31,7 @@ const Country = () => {
         {countries.map((country) => {
           return (
             <div
+              type="button"
               className="h-max p-3 border-2 shadow-lg rounded bg-white flex flex-col justify-around"
               key={country.country}
             >
@@ -28,16 +40,16 @@ const Country = () => {
                   <img
                     // width={200}
                     className="rounded"
-                    alt="Country's Flag"
+                    alt={`${country.country}'s flag`}
                     src={country.countryInfo.flag}
                   />
                 </div>
-                <div className="text-left h-40">
+                <div className="text-left h-max">
                   <div className="text-center md:text-left md:flex mb-5">
-                    <div className="text-xl md:text-2xl">Country: </div>
-                    <div className="text-xl md:text-2xl text-blue-900">
+                    <p className="text-xl md:text-2xl">Country: </p>
+                    <p className="text-xl md:text-2xl text-blue-900">
                       {country.country}
-                    </div>
+                    </p>
                   </div>
                   <p className="flex justify-between">
                     <span>Population: </span>
@@ -49,40 +61,61 @@ const Country = () => {
                   </p>
                 </div>
               </div>
-              <div className="p-3 grid gap-x-20 md:grid-cols-2 text-left bg-slate-300 rounded">
-                <p className="md:flex md:justify-between">
-                  <span>Total Cases: </span>
-                  <span>{country.cases}</span>
-                </p>
-                <p className="md:flex md:justify-between">
-                  <span>Total Recovered: </span>
-                  <span>{country.recovered}</span>
-                </p>
-                <p className="md:flex md:justify-between">
-                  <span>Today Cases: </span>
-                  <span>{country.todayCases}</span>
-                </p>
-                <p className="md:flex md:justify-between">
-                  <span>Today Recovered: </span>
-                  <span>{country.todayRecovered}</span>
-                </p>
-                <p className="md:flex md:justify-between">
-                  <span>Deaths: </span>
-                  <span>{country.deaths}</span>
-                </p>
-                <p className="md:flex md:justify-between">
-                  <span>Critical: </span>
-                  <span>{country.critical}</span>
-                </p>
-                <p className="md:flex md:justify-between">
-                  <span>Active: </span>
-                  <span>{country.active}</span>
-                </p>
-                <p className="md:flex md:justify-between">
-                  <span>Today Deaths: </span>
-                  <span>{country.todayDeaths}</span>
-                </p>
-              </div>
+              {show && currentCuntary === country.country ? (
+                <div>
+                  <button
+                    type="button"
+                    className="w-max text-white bg-slate-700 hover:bg-slate-300 rounded-lg px-6 py-1 mb-1"
+                    onClick={handleHide}
+                  >
+                    Hide Details
+                  </button>
+                  <div className="p-3 grid gap-x-20 md:grid-cols-2 text-left bg-slate-300 rounded">
+                    <p className="md:flex md:justify-between">
+                      <span>Total Cases: </span>
+                      <span>{country.cases}</span>
+                    </p>
+                    <p className="md:flex md:justify-between">
+                      <span>Total Recovered: </span>
+                      <span>{country.recovered}</span>
+                    </p>
+                    <p className="md:flex md:justify-between">
+                      <span>Today Cases: </span>
+                      <span>{country.todayCases}</span>
+                    </p>
+                    <p className="md:flex md:justify-between">
+                      <span>Today Recovered: </span>
+                      <span>{country.todayRecovered}</span>
+                    </p>
+                    <p className="md:flex md:justify-between">
+                      <span>Deaths: </span>
+                      <span>{country.deaths}</span>
+                    </p>
+                    <p className="md:flex md:justify-between">
+                      <span>Critical: </span>
+                      <span>{country.critical}</span>
+                    </p>
+                    <p className="md:flex md:justify-between">
+                      <span>Active: </span>
+                      <span>{country.active}</span>
+                    </p>
+                    <p className="md:flex md:justify-between">
+                      <span>Today Deaths: </span>
+                      <span>{country.todayDeaths}</span>
+                    </p>
+                  </div>
+                </div>
+              ) : (
+                <div className="flex justify-center m-0">
+                  <button
+                    type="button"
+                    className="w-max text-white bg-slate-700 hover:bg-slate-300 rounded-lg px-8 py-2"
+                    onClick={() => handleShow(country.country)}
+                  >
+                    Show Details{" "}
+                  </button>
+                </div>
+              )}
               <div className="md:flex justify-center gap-5 pt-3 leading-10 align-middle">
                 <span className="hidden md:flex mt-3">
                   <FaClock />
