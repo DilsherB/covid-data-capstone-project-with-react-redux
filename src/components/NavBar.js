1275a27abf059dccf93e45697b4e10cca6e808e5
Dilsher <dilsherb@gmail.com>
2023-07-04 11:31:21 +0300

    logo linked to home page

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,10 +10,19 @@ const Navbar = () => {
   };
 
   return (
-    <ul
-      className="flex justify-between mx-5 my-0.5 h-10 bg-slate-200 px-5 py-2 rounded-b mb-3"
-    >
-      <h3 className="text-3xl leading-5">Covid-19 Data</h3>
+    <ul className="flex justify-between mx-5 my-0.5 h-10 bg-slate-200 px-5 py-2 rounded-b mb-3">
+      <button
+        type="button"
+        onClick={() => setActiveItem("/")}
+        className={
+          activeItem === "/"
+            ? "text-slate-400 hover:text-slate-600 text-3xl leading-5"
+            : "hover:text-slate-600 text-3xl leading-5"
+        }
+        // "text-3xl leading-5"
+      >
+        <NavLink to="/">Covid-19 Data</NavLink>
+      </button>
       <div className="hidden md:flex gap-10">
         {Object.keys(menuItems).map((item) => (
           <button
